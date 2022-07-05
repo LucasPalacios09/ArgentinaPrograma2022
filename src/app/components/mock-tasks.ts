cdeb7001b61d61e70ce47c7742e9d97c93ec860f
Palacios <Lucas.Palacios@sovos.com>
2022-07-05 15:40:35 -0300

    Task agregado

---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Terminar primer modulo de Angular',
+    day: 'Agosto 5 a las 12:00',
+    remainder: true,
+  },
+  {
+    id: 2,
+    text: 'Terminar segundo modulo de Angular',
+    day: 'Agosto 6 a las 12:00',
+    remainder: true,
+  },
+  {
+    id: 3,
+    text: 'Terminar tercer modulo de Angular',
+    day: 'Agosto 7 a las 12:00',
+    remainder: false,
+  },
+  {
+    id: 4,
+    text: 'Terminar cuaarto modulo de Angular',
+    day: 'Agosto 8 a las 12:00',
+    remainder: false,
+  },
+];
